348adc2cacbe1ebee7ba2b000fcc8222c8b08edf
Kidus Makonnen <kidusmakonnen@gmail.com>
2020-02-06 01:17:38 +0300

    Use parameterized tests

--- src/main/java/et/kidus/geez/GeezUtil.java
+++ src/main/java/et/kidus/geez/GeezUtil.java
@@ -1,6 +1,9 @@
 package et.kidus.geez;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * A simple library to convert numbers to their Ge'ez number representation.
